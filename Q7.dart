8ce3bbe9afeee6b2fbdcd0c1627705bf9220e98c
ashir227 <ashirahmed810@gmail.com>
2024-02-05 07:56:22 -0800

    Q7

---
+++ Q7.dart
@@ -0,0 +1,11 @@
+void main() {
+  int number = 5;
+  print("Input: $number");
+
+  print("Output:");
+
+  for (int i = 1; i <= 10; i++) {
+    int result = number * i;
+    print("$number x $i = $result");
+  }
+}
